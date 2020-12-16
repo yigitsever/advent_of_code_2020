d434289da8f005d0d77de2c6ef721fa8f791e5ad
Yigit Sever <yigit@ceng.metu.edu.tr>
2020-12-16 18:57:38 +0300

    Add the answer to day 9 part 2

--- day9/xmas_encoder.pl
+++ day9/xmas_encoder.pl
@@ -2,36 +2,69 @@ use strict;
 use warnings;
 use DDP;
 use Smart::Comments;
+use Tie::File;
+use List::Util qw(min max);
 
-open my $fh, '<', "input" or die "no input present, $!";
+tie my @xmas, 'Tie::File', "input" or die "no input present, $!";
 
 my %preamble;
 # because it's easier this way trust me
 my @also_queue;
+my $goalnum;
 
-foreach (1..25) {
-    my $t = int(<$fh>);
-    $preamble{$t} = 1;
-    push @also_queue, int($t);
-}
+foreach my $idx (0 .. $#xmas) {
+    if ($idx < 25) {
+        my $t = int($xmas[$idx]);
+        $preamble{$t} = 1;
+        push @also_queue, $t;
+    } else {
+        my $nextnum = int($xmas[$idx]);
+        my $tester = 0;
+        foreach my $num (keys %preamble) {
+            $tester++;
+            if (exists $preamble{$nextnum - $num}) {
+                my $old = shift @also_queue;
+                delete $preamble{$old};
+                push @also_queue, $nextnum;
+                $preamble{$nextnum} = 1;
+                last;
+            }
+        }
 
-while (my $nextnum = <$fh>) {
-    $nextnum = int($nextnum);
-    my $tester = 0;
-    foreach my $num (keys %preamble) {
-        $tester++;
-        if (exists $preamble{$nextnum - $num}) {
-            my $old = shift @also_queue;
-            delete $preamble{$old};
-            push @also_queue, $nextnum;
-            $preamble{$nextnum} = 1;
+        if (not scalar grep { $_ == $nextnum } @also_queue) {
+            $goalnum = $nextnum;
+            print("XMAS weak num: $goalnum\n");
             last;
         }
     }
+}
 
-    if (not scalar grep { $_ == $nextnum } @also_queue) {
-        print($nextnum);
+# find the contiguous set
+
+my @contiguous;
+my $total = 0;
+
+foreach my $curr (@xmas) {
+    if ($total + $curr < $goalnum) {
+        $total += $curr;
+        push @contiguous, $curr;
+    } elsif ($total + $curr > $goalnum) {
+        while ($total + $curr > $goalnum) {
+            my $evictee = shift @contiguous;
+            if (not defined $evictee) {
+                last;
+            }
+            $total -= $evictee;
+        }
+        push @contiguous, $curr;
+        $total += $curr;
+    }
+
+    if ($total == $goalnum) {
+        print(min(@contiguous) + max(@contiguous));
+        p @contiguous;
         exit;
     }
 }
 
+untie @xmas;
